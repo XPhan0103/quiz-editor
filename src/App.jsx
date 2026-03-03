5273e598302ab23a515a4210ae6c11d364559de1
NhanPhan <ptn987654321@gmail.com>
2026-03-04 00:58:46 +0700

    feat: create questionlist component and render it in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import QuizInfo from './components/QuizInfo';
+import QuestionList from "./components/QuestionList";
 import './index.css';
 
 function App() {
@@ -10,11 +11,16 @@ function App() {
     questions: [],
   });
 
+  const setQuestions = (newQuestions) => {
+    setQuizData((prev) => ({ ...prev, questions: newQuestions }));
+  };
+
   return (
     <div className="app">
       <Header />
       <div className="main-content">
         <QuizInfo quizData={quizData} setQuizData={setQuizData} />
+        <QuestionList questions={quizData.questions} setQuestions={setQuestions} />
       </div>
     </div>
   );
